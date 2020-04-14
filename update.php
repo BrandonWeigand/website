13081f0d57278a0617770f716d8cfc5932e7b590
brandonweigand <brandonweigand@gmail.com>
2020-04-14 01:57:34 -0400

    test 4

--- update.php
+++ update.php
@@ -1,7 +1,7 @@
 <?php 
 ob_start();
-//passthru("<i>command</i>");
-passthru("git pull;git merge;"););
+passthru("<i>command</i>");
+//passthru("git pull;git merge;"););
 $var = ob_get_contents();
 ob_end_clean(); //Use this instead of ob_flush()
 ?>
