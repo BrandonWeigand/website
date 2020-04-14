e0974862c14f0918d0dedfa274cf0a5e1bd4577a
brandonweigand <brandonweigand@gmail.com>
2020-04-14 01:59:53 -0400

    test6

--- update.php
+++ update.php
@@ -1,7 +1,15 @@
 <?php 
-ob_start();
-passthru("<i>git pull;git merge;</i>");
 //passthru("git pull;git merge;"););
-$var = ob_get_contents();
-ob_end_clean(); //Use this instead of ob_flush()
+echo '<pre>';
+
+// Outputs all the result of shellcommand "ls", and returns
+// the last output line into $last_line. Stores the return value
+// of the shell command in $retval.
+$last_line = system('ls', $retval);
+
+// Printing additional info
+echo '
+</pre>
+<hr />Last line of the output: ' . $last_line . '
+<hr />Return value: ' . $retval;
 ?>
