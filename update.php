76eeec571ccf4d30d475b8b65d46426f915f9948
brandonweigand <brandonweigand@gmail.com>
2020-04-14 02:00:50 -0400

    test 7

--- update.php
+++ update.php
@@ -2,10 +2,7 @@
 //passthru("git pull;git merge;"););
 echo '<pre>';
 
-// Outputs all the result of shellcommand "ls", and returns
-// the last output line into $last_line. Stores the return value
-// of the shell command in $retval.
-$last_line = system('ls', $retval);
+$last_line = system('git pull;git merge;', $retval);
 
 // Printing additional info
 echo '
