4993e9a9c0bc87d48fe93aa8151a40c1bb1d4128
brandonweigand <brandonweigand@gmail.com>
2020-04-14 02:29:35 -0400

    asldk

--- update.php
+++ update.php
@@ -3,13 +3,12 @@
 echo '<pre>';
 $cd = __DIR__;
 $cmd = "cd  $cd;git pull -v 2>&1;";
-$last_line = passthru ($cmd, $retval);
+$last_line = shell_exec ($cmd);
 
 // Printing additional info
 echo '
 </pre>
-<hr />Last line of the output: ' . $last_line . '
-<hr />Return value: ' . $retval.
+<hr />Last line of the output: ' . $last_line . 
 '<hr>Path: '.$cd.
 '<hr>CMD: '.$cmd;
 ?>
