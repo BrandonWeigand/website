95f4aa703fe159b34049d8b1bc6a60d07405da69
brandonweigand <brandonweigand@gmail.com>
2020-04-14 02:07:21 -0400

    test0

--- update.php
+++ update.php
@@ -2,7 +2,7 @@
 //passthru("git pull;git merge;"););
 echo '<pre>';
 
-$last_line = system('git pull -v;top;', $retval);
+$last_line = system('git pull -verbose;', $retval);
 
 // Printing additional info
 echo '
