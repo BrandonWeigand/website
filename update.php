e15418e4d34f01d9404bfd03005af702301ea130
brandonweigand <brandonweigand@gmail.com>
2020-04-15 02:32:43 -0400

    deploy

--- update.php
+++ update.php
@@ -6,8 +6,8 @@ $url = "https://github.com/BrandonWeigand/website.git";
 $branch = "master";
 $ownme="cd $cd".'sudo chown -R $USER: ./;sudo chmod -R 777 ./;';
 $gitcmd=array(
-    "pull"=>"sudo git pull -v $url $branch;",
-    "clone"=>"rm -rfv $cd;sudo git clone $url $cd;"
+    "pull"=>"sudo git pull -v $url $branch 2>&1;",
+    "clone"=>"rm -rfv $cd;sudo git clone $url $cd 2>&1;"
 );
 $cmd = "{$ownme}{$gitcmd[$_GET["git"]]}";
 if(isset($_GET["git"])){
